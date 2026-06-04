9ba98b8834de57fbb95d66205c809706da5f2f4e
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-06-04 21:37:42 +0200

    Change task list items

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,21 @@
 import { twMerge } from "tailwind-merge";
-import { useTasks, type Status, type Task } from "../context/useTasks";
-import type { ComponentProps } from "react";
 import { useDraggable, useDroppable } from "@dnd-kit/react";
+import { closestCenter } from "@dnd-kit/collision"
 
-type TaskListProps = { 
+import { useTasks, type Status, type Task } from "../context/useTasks";
+import { Button } from "./Button";
+
+type TaskListProps = {
     name: string,
     status: Status
-} & ComponentProps<"div">
+}
 
-export function TaskList({ name, status, className }: TaskListProps) {
+export function TaskList({ name, status }: TaskListProps) {
     const { tasks } = useTasks()
-    const { ref } = useDroppable({ id: status.toString() });
+    const { ref } = useDroppable({
+        id: status.toString(),
+        collisionDetector: closestCenter
+    })
     const visibleTasks = tasks.filter(task => task.status === status)
 
     return (
@@ -20,7 +25,6 @@ export function TaskList({ name, status, className }: TaskListProps) {
                 "flex flex-col gap-3 rounded-xl p-4 text-zinc-100",
                 "bg-blue-950/40 backdrop-blur-sm border-2",
                 getStatustyling(status),
-                className
             )}
         >
             <h2 className="text-sm font-semibold uppercase tracking-wider text-zinc-400 px-1">
@@ -42,20 +46,23 @@ function TaskListItem({ task }: TaskListItemProps) {
     return (
         <div
             ref={ref}
-            className="flex flex-col gap-1 rounded-lg bg-blue-900/20 border border-blue-900/40 px-4 py-3 shadow-sm hover:border-blue-800/60 transition-colors">
-            <h3 className="font-medium text-zinc-200 text-sm">{task.name}</h3>
-            <div className="text-xs text-zinc-500 flex gap-1">
-                Create: 
-                <time dateTime={task.createdAt.toISOString()}>
-                    {task.createdAt.toLocaleDateString()}
-                </time>
+            className="flex justify-between rounded-lg bg-blue-900/20 border border-blue-900/40 px-4 py-3 shadow-sm hover:border-blue-800/60 transition-colors">
+            <div className="flex flex-col gap-1 ">
+                <h3 className="font-medium text-zinc-200 text-sm">{task.name}</h3>
+                <div className="text-xs text-zinc-500 flex gap-1">
+                    Create:
+                    <time dateTime={task.createdAt.toISOString()}>
+                        {task.createdAt.toLocaleDateString()}
+                    </time>
+                </div>
             </div>
+            <Button>Delete</Button>
         </div>
     )
 }
 
 function getStatustyling(status: Status) {
-    switch(status) {
+    switch (status) {
         case "open":
             return "border-emerald-500/30 shadow-lg shadow-emerald-500/5";
         case "progress":
