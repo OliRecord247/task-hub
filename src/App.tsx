88b14c42dad1cf6d11e72a5441db84ca77ae81cb
Oliver Verdesseldonck <oliver.verdesseldonck@gmail.com>
2026-06-04 18:50:50 +0200

    using task list and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import { TaskProvider } from "./context/TaskProvider";
 
 export default function App() {
   return (
-    <div>
-      <Header />
+    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+      <TaskProvider>
+        <Header />
+        <TaskList name="Open" status="open" />
+        <TaskList name="Progress" status="progress" />
+        <TaskList name="Closed" status="closed" />
+      </TaskProvider>
     </div>
   )
 }
